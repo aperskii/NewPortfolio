bcdede5f050b2657ad655de6b26dc79ea13969d1
Yassine Berqiqch <yassine.berqiqch@gmail.com>
2022-03-26 16:00:50 +0100

    yes

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PortFolio Yassine</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header class="header">
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+    <a href="#home" class="logo">PortFolio</a>
+    <nav class="navbar">
+        <a href="#home" class="active">home</a>
+        <a href="#about">about</a>
+        <a href="#services">services</a>
+        <a href="#Portfolio">Portfolio</a>
+        <a href="#contact">contact</a>
+    </nav>
+<div class="follow">
+    <a href="#" class="fab fa-facebook-f"></a>
+    <a href="#" class="fab fa-twitter"></a>
+    <a href="#" class="fab fa-instagram"></a>
+    <a href="#" class="fab fa-linkedin"></a>
+    <a href="#" class="fab fa-github"></a>
+</div>
+
+
+</header>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="js/script.js"></script>
+
+</body>
+</html> 
